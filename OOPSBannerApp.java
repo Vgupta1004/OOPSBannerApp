000d517514558ef9c95d9898a6c9f0a3237f0b4f
gupta <vgup3012@7-11.com>
2026-02-17 14:22:35 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+* OOPSBannerApp UC1 - OOPS Banner App
+* Print OOPS
+*
+*
+* @author Developer
+* @version 1.0
+*/
+
+class OOPSBannerApp{
+	public static void main(String args[]){
+		System.out.println("OOPS");
+	}
+}
