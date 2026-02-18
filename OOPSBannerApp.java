47633da426b6356628ef3b0b9e0b19edafc3e0f0
gupta <vgup3012@7-11.com>
2026-02-18 15:58:22 +0530

    Coded UC5 to display OOPS Banner using Array Inline initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -6,15 +6,7 @@
 
 class OOPSBannerArray{
 	public static void main(String args[]){
-		String arr[] = new String[7];
-		arr[0] = String.join(" ","  ***  ","  ***  ","****  ","  ***** ");
-		arr[1] = String.join(" "," ** ** "," ** ** ","**  **"," **   **");
-		arr[2] = String.join(" ","**   **","**   **","**  **","**      ");
-		arr[3] = String.join(" ","**   **","**   **","****  ","  ***** ");
-		arr[4] = String.join(" ","**   **","**   **","**    ","      **");
-		arr[5] = String.join(" "," ** ** "," ** ** ","**    ","**   ** ");
-		arr[6] = String.join(" ","  ***  ","  ***  ","**    "," *****  ");
-		
+		String arr[] = {String.join(" ","  ***  ","  ***  ","****  ","  ***** "), String.join(" "," ** ** "," ** ** ","**  **"," **   **"), String.join(" ","**   **","**   **","**  **","**      "), String.join(" ","**   **","**   **","****  ","  ***** "), String.join(" ","**   **","**   **","**    ","      **"), String.join(" "," ** ** "," ** ** ","**    ","**   ** "), String.join(" ","  ***  ","  ***  ","**    "," *****  ");}
 		System.out.println();
 		for(String s:arr){
 			System.out.println(s);
