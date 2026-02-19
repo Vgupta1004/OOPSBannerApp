b4227a847109fa0024b08689f45c6af817bccbb6
Vgupta1004 <98306984+Vgupta1004@users.noreply.github.com>
2026-02-19 10:14:00 +0530

    Update OOPSBannerApp with comments and version
    Added documentation and updated version number.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,9 @@
+/**
+* This program uses '+' operator to concat 2 strings and display OOPS Banner in console
+* @author Developer
+* @version 2.0
+*/
+
 class OOPSBannerApp{
 	public static void main(String args[]){
 		System.out.println();
